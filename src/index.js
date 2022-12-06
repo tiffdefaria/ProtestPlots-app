ed354e4e2d38d7899dc97ad82d110ec44f818034
Tiffany De Faria <tdefaria00@gmail.com>
2022-12-06 00:52:58 -0500

    User login functionality added

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App'
 import ProtestPg from './ProtestPg';
 import Login from './Login';
 import Register from './Register'
+import Dashboard from './dashboard'
 
 ReactDOM.render(
   <BrowserRouter>
@@ -13,6 +14,7 @@ ReactDOM.render(
         <Route path="/protestpg" element={<ProtestPg/>} />
         <Route path="/login" element={<Login/>} />
         <Route exact path="/register" element={<Register />} />
+        <Route exact path="/dashboard" element={<Dashboard />} />
     </Routes>
   </BrowserRouter>,
   document.getElementById('root')
